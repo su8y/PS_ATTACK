5024d56a502b016535d96ea855b62141cc516931
3uby <bsa0530@gmail.com>
2022-12-25 17:43:54 +0900

    [BOJ/상호배타적집합]거짓말
    처음에는 dfs 로 모든 경우에 수를 돌면서 확인을 하려고 했으나 양방향으로 같은일을 반복해야하는 일 때문에
    최적화를 할수있는 방법이 뭐가 있을까 생각하다가 거짓말을 진실을 들은 사람들은 같은 집합의 소속된다는 것을
    생각하여서 집합 자료구조를 사용하여 풀었다.
    
    https://www.acmicpc.net/problem/1043

--- korsua/Main.java
+++ korsua/Main.java
@@ -6,9 +6,11 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
+    
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+        
 
     }
 }
