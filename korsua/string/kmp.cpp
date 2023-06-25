2bc5ea168b57abcdfe292ffc85a718e20b74ae85
SUBY <bsa0530@gmail.com>
2023-06-26 01:43:02 +0900

    :zap: [BOJ/KMP]1786 찾기

---
+++ korsua/string/kmp.cpp
@@ -0,0 +1,73 @@
+#include "bits/stdc++.h"
+
+using namespace std;
+#ifdef BOJ
+#define debug(x)  ((void)0)
+#define debugArr(c) ((void)0)
+#else
+#define debug(x) std::cout << "[DEBUG] " << #x <<" is " << x << '\n'
+#define debugArr(arr){ std::cout << "[DEBUG] "<< #arr << ": "; for (const auto& elem : arr) cout << elem << ", "; cout << '\n';}
+#endif
+#define FOR(i, N) for(int i=0; i<N;i++)
+#define FFOR(i, N) for(int i=1; i<=N;i++)
+#define FORN(N) while(N--)
+#define FAST cin.tie(NULL);cout.tie(NULL); ios::sync_with_stdio(false)
+#define ll long long
+#define endl "\n"
+typedef pair<int, int> pii;
+
+int T, N, M, j;
+string pattern, answer;
+vector<int> pi;
+vector<int> result;
+
+
+int main() {
+    FAST;
+    getline(cin, answer);
+    cin >> pattern;
+    pi.resize(pattern.length() + 1);
+
+    int i = 1,j = 0;
+    while(i +j < pattern.length()){
+        if(pattern[i+j] == pattern[j]){
+
+        ++j;
+        pi[i+j-1]=j;
+        }else{
+            if(j==0){
+                ++i;
+            }else{
+                i += j - pi[j-1];
+                j = pi[j - 1];
+            }
+        }
+    }
+
+
+    int begin =0, matched=0;
+    while (begin <= answer.length() - pattern.length()) {
+        if(matched < pattern.length() && answer[begin+matched] == pattern[matched]){
+            ++matched;
+            if(matched == pattern.length()) result.push_back(begin);
+        }
+        else{
+            if(matched==0){
+                ++begin;
+            }else{
+                begin += matched - pi[matched -1];
+                matched = pi[matched -1];
+            }
+        }
+
+    }
+
+    cout << result.size() << endl;
+    for (auto &i: result) {
+        cout << i + 1 << " ";
+    }
+
+
+    return 0;
+}
+
