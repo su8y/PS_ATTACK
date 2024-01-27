5d64a9720cfbcfd3aa1b07a91201f40450e3be23
tecte <121792901+tecte1023@users.noreply.github.com>
2024-01-28 01:06:42 +0900

    Tecte / 6문제 (#48)
    * [백준/그리디] 주사위 (1041)
    
    * [백준/DFS] 알파벳 (1987)
    
    * [백준/Tree DP] 뉴스 전하기 (1135)
    
    * [백준/Tree DP] 뉴스 전하기 (1135)
    
    * [백준/DP] 팰린드롬 분할(1509)
    
    * [백준/수학] 평균은 넘겠지 (4344)
    
    * [백준/수학] 터렛 (1002)

---
+++ tecte/Baekjoon/뉴스전하기.java
@@ -0,0 +1,56 @@
+/*
+ * 문 제 : 백준 1135번 - 뉴스 전하기 (https://www.acmicpc.net/problem/1135)
+ * 문제 풀이
+ *  ArrayList<HashMap<>>을 사용하여 인덱스가 직원i인 HashMap에는 키로 직원i의 직속 부하들, 값으로 직원i부터 직원i의 직·간접적인 부하까지 연락하는데 걸리는 시간을 저장
+ *  HashMap 리스트의 끝에서부터 HashMap의 값을 기준으로 정렬하고, 값 + 연락 순서를 계산하여 최대값을 자신을 키로 하는 HashMap에 저장
+ *  예를 들어, 값이 1, 3, 5가 있으면 내림차순으로 정렬하고 5, 3, 1 가장 오래 걸리는 값인 5부터 연락을 하면 5+1, 3+2, 1+3 으로 이 중 최대값인 6이 저장됨
+ */
+
+package Baekjoon;
+
+import java.io.*;
+import java.util.*;
+
+// public class  Main {
+public class 뉴스전하기 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+        ArrayList<HashMap<Integer, Integer>> list = new ArrayList<>(n);
+        String[] input = br.readLine().split(" ");
+        int[] superior = new int[n];
+
+        // 초기화
+        for (int i = 0; i < n; i++)
+            list.add(new HashMap<>());
+
+        for (int i = 1; i < n; i++) {
+            int p = Integer.parseInt(input[i]);
+
+            superior[i] = p; // 자신의 직속 상관 저장
+            list.get(p).put(i, 0); // list에 자신의 직속 부하 추가
+        }
+
+        for (int i = n - 1; i >= 0; i--) {
+            // HashMap의 값을 기준으로 정렬
+            List<Map.Entry<Integer, Integer>> entryList = new LinkedList<>(list.get(i).entrySet());
+            entryList.sort(Map.Entry.comparingByValue());
+
+            int seq = list.get(i).size();
+            int max = 0;
+
+            // HashMap의 값 + 연락순서인데 정렬을 오름차순으로 했기 때문에 나중 순서부터 더해서 최대값 비교
+            for (Map.Entry<Integer, Integer> entry : entryList)
+                max = Math.max(max, entry.getValue() + seq--);
+
+            list.get(superior[i]).put(i, max); // 최대값을 자신을 키로 하는 HashMap에 저장
+        }
+
+        bw.write(Integer.toString(list.get(0).get(0)));
+        bw.flush();
+
+        br.close();
+        bw.close();
+    }
+}
---
+++ tecte/Baekjoon/알파벳.java
@@ -0,0 +1,59 @@
+/*
+ * 문 제 : 백준 1987번 - 알파벳 (https://www.acmicpc.net/problem/1987)
+ * 문제 풀이
+ *  DFS을 사용하여 다음 이동할 곳의 알파벳이 이미 지나온 길의 알파벳인지 확인하고 지나온 알파벳이 아니면 이동, 지나온 알파벳이면 다른 곳을 확인하여 이동
+ */
+
+package Baekjoon;
+
+import java.io.*;
+import java.util.stream.Stream;
+
+// public class  Main {
+public class 알파벳 {
+    static int r;
+    static int c;
+    static int[][] board;
+    static int[] dx = {-1, 0, 1, 0};
+    static int[] dy = {0, 1, 0, -1};
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String[] input = br.readLine().split(" ");
+        r = Integer.parseInt(input[0]);
+        c = Integer.parseInt(input[1]);
+        board = new int[r][c];
+
+        // 알파벳을 아스키 코드로 바꿔 저장
+        for (int i = 0; i < r; i++)
+            board[i] = Stream.of(br.readLine().split("")).flatMapToInt(String::chars).toArray();
+
+        bw.write(Integer.toString(move(0, 0, 0))); // 재귀함수로 이동한 칸 수를 구함
+        bw.flush();
+
+        br.close();
+        bw.close();
+    }
+
+    public static int move(int x, int y, int p) {
+        p |= 1 << (board[x][y] - (int) 'a'); // 비트 마스크를 사용하여 현재 칸을 이동경로에 저장
+
+        int moveCnt = Integer.bitCount(p); // 이진수에서 1의 개수를 구함, 1의 개수가 이동한 칸 수
+
+        for (int i = 0; i < dx.length; i++) {
+            int mx = x + dx[i];
+            int my = y + dy[i];
+
+            // 이동한 곳이 보드의 범위 안에 없으면 continue
+            if (mx < 0 || my < 0 || mx >= r || my >= c)
+                continue;
+
+            // 비트 마스크를 사용하여 이동할 곳의 알파벳이 이동한 경로의 알파벳과 겹치지 않을 경우 재귀함수 호출
+            if ((p & (1 << (board[mx][my] - (int) 'a'))) == 0)
+                moveCnt = Math.max(move(mx, my, p), moveCnt); // 이동할 수 있는 곳 중 비교하여 가장 많이 이동한 칸 수 저장
+        }
+
+        return moveCnt;
+    }
+}
