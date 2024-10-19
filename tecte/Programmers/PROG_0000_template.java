276dc7e53ce17a5f80cb2a88ed2f306365879117
tecte <tecte1023@gmail.com>
2024-10-19 17:21:44 +0900

    템플릿 업데이트

--- tecte/Programmers/PROG_0000_template.java
+++ tecte/Programmers/PROG_0000_template.java
@@ -1,24 +1,14 @@
 /*
- * 문제 : 프로그래머스 - 0000 (https://school.programmers.co.kr/learn/courses/30/lessons/0000)
+ * 문제: 프로그래머스 - 0000 (https://school.programmers.co.kr/learn/courses/30/lessons/0000)
  * 문제 풀이
  *
+ *
+ * 시간 복잡도:
  */
 
 package Programmers;
 
 public class PROG_0000_template {
-    static class Solution {
-        public int solution() {
+    // class Solution {
 
-
-            return 0;
-        }
-    }
-
-    public static void main(String[] args) {
-        int[] n = {};
-
-        for (int i = 0; i < n.length; i++)
-            System.out.println(new Solution().solution());
-    }
 }
