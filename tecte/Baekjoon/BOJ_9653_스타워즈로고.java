c3050dcadd003b948fae172364b55e9a538ad298
tecte <tecte1023@gmail.com>
2024-10-19 17:57:39 +0900

    9653. 스타워즈 로고

---
+++ tecte/Baekjoon/BOJ_9653_스타워즈로고.java
@@ -0,0 +1,28 @@
+/*
+ * 문제: 백준 9653번 - 스타워즈 로고 (https://www.acmicpc.net/problem/9653)
+ */
+
+package Baekjoon;
+
+import java.io.*;
+
+public class BOJ_9653_스타워즈로고 {
+    // public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        bw.write("    8888888888  888    88888\n");
+        bw.write("   88     88   88 88   88  88\n");
+        bw.write("    8888  88  88   88  88888\n");
+        bw.write("       88 88 888888888 88   88\n");
+        bw.write("88888888  88 88     88 88    888888\n\n");
+        bw.write("88  88  88   888    88888    888888\n");
+        bw.write("88  88  88  88 88   88  88  88\n");
+        bw.write("88 8888 88 88   88  88888    8888\n");
+        bw.write(" 888  888 888888888 88  88      88\n");
+        bw.write("  88  88  88     88 88   88888888\n");
+        br.close();
+        bw.close();
+    }
+}
